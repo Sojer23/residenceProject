2ad1ef638a9d6f12b70d7330535806dbc11f6b2e
José Gallardo <josegallardog95@gmail.com>
2017-11-06 19:11:47 +0100

    Added post

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@
 
 
 //Module dependences//
-var express = require('express'),
-    helmet = require('helmet'),
-    http = require('http');
+var express = require('express');
+var helmet = require('helmet');
+var body_parser = require('body-parser');
+var http = require('http');
+
+//var routes = require('./routes.js');
 
 //Files dependences//
 var config = require('./configurations/config.js');
@@ -12,6 +15,7 @@ var config = require('./configurations/config.js');
 
 var app = express();
 app.use(helmet());
+app.use(body_parser.json());
 app.use('/', express.static(__dirname + '/../public'));
 //app.use('/api', routes);
 
@@ -27,9 +31,9 @@ function _deploy(callback) {
 
     config.dbConnection(app, function () {
         var serverPort = process.env.PORT || config.server.port;
-        /* if (!module.exports.server) {
-             module.exports.server = http.createServer(app);
-         }*/
+        if (!module.exports.server) {
+            module.exports.server = http.createServer(app);
+        }
 
         app.listen(serverPort, () => {
             console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
