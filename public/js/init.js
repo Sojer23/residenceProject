154ab589b71ecef364f8de7fc6cfa35f898a22e4
José Gallardo <josegallardog95@gmail.com>
2017-12-03 21:40:05 +0100

    Compressed menu added and css

---
+++ public/js/init.js
@@ -0,0 +1,8 @@
+(function ($) {
+    $(function () {
+        $('select').material_select();
+        $('.modal-trigger').modal();
+        $('.button-collapse').sideNav();
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
